e4d4cb5b63dcf4d4427550c3738a8fc99afe5acc
yurdeth <angelvasquez2570@gmail.com>
2024-10-15 02:29:24 -0600

    Add QueryHelper

---
+++ OptiFinance_System/database/helper/QueryHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Message = OptiFinance_System.utils.Message;
+
+namespace OptiFinance_System.database.helper;
+
+public class QueryHelper
+{
+    public static bool ExecuteNonQuery(SqlConnection connection, string query, List<SqlParameter> parameters,
+        SqlTransaction? transaction = null)
+    {
+        try
+        {
+            using (SqlCommand command = new SqlCommand(query, connection, transaction))
+            {
+                command.Parameters.AddRange(parameters.ToArray());
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+        catch (Exception e)
+        {
+            Message.MessageViewError(@"Error al ejecutar la consulta SQL: " + e.Message);
+            return false;
+        }
+    }
+}
