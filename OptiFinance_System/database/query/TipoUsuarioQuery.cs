ef97229d5f066220cb09d991782aaf865e8fedd5
yurdeth <angelvasquez2570@gmail.com>
2024-10-19 01:42:09 -0600

    Finalizando de manera parcial consultas para los tipos de usuarios

--- OptiFinance_System/database/query/TipoUsuarioQuery.cs
+++ OptiFinance_System/database/query/TipoUsuarioQuery.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using OptiFinance_System.database.connection;
+using OptiFinance_System.database.generalities;
+using OptiFinance_System.database.generalities.parameters;
 using OptiFinance_System.database.helper;
 using OptiFinance_System.database.interfaces;
 using OptiFinance_System.database.models;
@@ -9,39 +11,53 @@ namespace OptiFinance_System.database.query;
 public class TipoUsuarioQuery : IQueryEstandar<TipoUsuario>
 {
     private static readonly Lazy<TipoUsuarioQuery> _instance = new(() => new());
-
     private readonly Connection _connectionInstance;
 
     private TipoUsuarioQuery()
     {
         _connectionInstance = Connection.Instance;
+        _connectionInstance.OpenConnection();
     }
 
     public static TipoUsuarioQuery Instance => _instance.Value;
+    private static TipoUsuarioParams Params => Queries.TipoUsuarioParams;
 
     public bool Insert(TipoUsuario entity, SqlTransaction? transaction = null)
     {
-        throw new NotImplementedException();
+        return QueryHelper.ExecuteInsert(_connectionInstance.GetSqlConnection(), Params.SqlInsert,
+            Params.ParametersInsert(entity), transaction);
     }
 
     public bool Insert(List<TipoUsuario> entities)
     {
-        throw new NotImplementedException();
+        return QueryHelper.ExecuteInTransaction(_connectionInstance.GetSqlConnection(), transaction =>
+        {
+            return entities.Select(entity => Params.ParametersInsert(entity)).Select(parameters =>
+                QueryHelper.ExecuteInsert(_connectionInstance.GetSqlConnection(), Params.SqlInsert, parameters,
+                    transaction)).All(result => result);
+        });
     }
 
     public bool Update(TipoUsuario entity, SqlTransaction? transaction = null)
     {
-        throw new NotImplementedException();
+        return QueryHelper.ExecuteUpdate(_connectionInstance.GetSqlConnection(), Params.SqlUpdate,
+            Params.ParametersUpdate(entity), transaction);
     }
 
     public bool Update(List<TipoUsuario> entities)
     {
-        throw new NotImplementedException();
+        return QueryHelper.ExecuteInTransaction(_connectionInstance.GetSqlConnection(), transaction =>
+        {
+            return entities.Select(entity => Params.ParametersUpdate(entity)).Select(parameters =>
+                QueryHelper.ExecuteUpdate(_connectionInstance.GetSqlConnection(), Params.SqlUpdate, parameters,
+                    transaction)).All(result => result);
+        });
     }
 
     public bool Delete(long id, SqlTransaction? transaction = null)
     {
-        throw new NotImplementedException();
+        return QueryHelper.ExecuteDelete(_connectionInstance.GetSqlConnection(), Params.SqlDelete,
+            Params.ParametersDelete(id), transaction);
     }
 
     public bool Delete(TipoUsuario entity)
@@ -51,7 +67,12 @@ public class TipoUsuarioQuery : IQueryEstandar<TipoUsuario>
 
     public bool Delete(List<long> ids)
     {
-        throw new NotImplementedException();
+        return QueryHelper.ExecuteInTransaction(_connectionInstance.GetSqlConnection(), transaction =>
+        {
+            return ids.Select(id => Params.ParametersDelete(id)).Select(parameters =>
+                QueryHelper.ExecuteDelete(_connectionInstance.GetSqlConnection(), Params.SqlDelete, parameters,
+                    transaction)).All(result => result);
+        });
     }
 
     public bool Delete(List<TipoUsuario> entities)
@@ -61,26 +82,17 @@ public class TipoUsuarioQuery : IQueryEstandar<TipoUsuario>
 
     public TipoUsuario? FindById(long id)
     {
-        string query = "SELECT id, nombre FROM tipo_usuario WHERE id = @id";
-        List<SqlParameter> parameters = new()
-        {
-            new("@id", id)
-        };
-        return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), query, MapEntity, parameters);
+        return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlFindById, MapEntity,
+            Params.ParametersFindById(id));
     }
 
     public List<TipoUsuario> SelectAll()
     {
-        string query = "SELECT id, nombre FROM tipo_usuario";
-        return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), query, MapEntity);
+        return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAll, MapEntity);
     }
 
     public TipoUsuario MapEntity(SqlDataReader reader)
     {
-        return new()
-        {
-            Id = reader.GetInt64(0),
-            Nombre = reader.GetString(1)
-        };
+        return Params.Map(reader);
     }
 }
