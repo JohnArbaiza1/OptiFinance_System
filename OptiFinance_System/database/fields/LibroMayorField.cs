2c0ec99f64766597e0ca99e99ae3a039359ea7b8
yurdeth <angelvasquez2570@gmail.com>
2024-10-31 01:26:22 -0600

    Sql del libro  mayor

---
+++ OptiFinance_System/database/fields/LibroMayorField.cs
@@ -0,0 +1,11 @@
+﻿namespace OptiFinance_System.database.fields;
+
+public class LibroMayorField
+{
+    public const int Id = 0;
+    public const int Debe = 1;
+    public const int Haber = 2;
+    public const int IdCuenta = 3;
+    public const int Codigo = 4;
+    public const int Nombre = 5;
+}
---
+++ OptiFinance_System/database/generalities/parameters/LibroMayorParams.cs
@@ -0,0 +1,86 @@
+﻿using OptiFinance_System.global;
+using Microsoft.Data.SqlClient;
+using OptiFinance_System.database.fields;
+using OptiFinance_System.database.models;
+
+namespace OptiFinance_System.database.generalities.parameters;
+
+public class LibroMayorParams
+{
+    public string SqlInsert { get; }
+    public string SqlUpdate { get; }
+    public string SqlDelete { get; }
+    public string SqlFindById { get; }
+    public string SqlSelectAllByPartida { get; }
+    public string SqlSearchAll { get; }
+
+    public string SqlSelectAll =>
+        "SELECT r.id, r.debe, r.haber, c.id AS id_cuenta, c.codigo AS codigo, c.nombre AS nombre FROM registros AS r " +
+        "INNER JOIN cuentas AS c ON r.id_cuenta = c.id " +
+        "INNER JOIN empresas ON c.id_empresa = empresas.id " + 
+        "WHERE empresas.id = @id_empresa";
+    
+    public string SelectAllByCodigo =>
+        "SELECT r.id, r.debe, r.haber, c.id AS id_cuenta, c.codigo AS codigo, c.nombre AS nombre FROM registros AS r " +
+        "INNER JOIN cuentas AS c ON r.id_cuenta = c.id " +
+        "INNER JOIN empresas ON c.id_empresa = empresas.id " + 
+        "WHERE c.codigo = @codigo AND empresas.id = @id_empresa";
+
+    public List<SqlParameter> ParametersInsert(LibroMayor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<SqlParameter> ParametersUpdate(LibroMayor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<SqlParameter> ParametersDelete(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<SqlParameter> ParametersFindById(long id, Usuario? user = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<SqlParameter> ParametersSearchAll(string search)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<SqlParameter> ParametersSelectAll()
+    {
+        return new()
+        {
+            new("@id_empresa", Global.SelectedEmpresa?.Id ?? 0)
+        };
+    }
+    
+    public List<SqlParameter> ParametersSelectAllByAccount(long id)
+    {
+        return new()
+        {
+            new("@id_empresa", Global.SelectedEmpresa?.Id ?? 0),
+            new("@id_cuenta", id)
+        };
+    }
+
+    public LibroMayor Map(SqlDataReader reader)
+    {
+        return new()
+        {
+            Id = reader.GetInt64(LibroMayorField.Id),
+            Debe = reader.GetDecimal(LibroMayorField.Debe),
+            Haber = reader.GetDecimal(LibroMayorField.Haber),
+            Cuenta = new()
+            {
+                Id = reader.GetInt64(LibroMayorField.IdCuenta),
+                Codigo = reader.GetString(LibroMayorField.Codigo),
+                Nombre = reader.GetString(LibroMayorField.Nombre)
+            }
+        };
+    }
+}
