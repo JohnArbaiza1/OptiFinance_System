4d227769d21834d38d59b7c13a91bfe82570404c
yurdeth <angelvasquez2570@gmail.com>
2024-11-02 20:57:25 -0600

    Reestructuracion de clases consultas en BalanceGeneralQuery

--- OptiFinance_System/Views/balanceGeneral.cs
+++ OptiFinance_System/Views/balanceGeneral.cs
@@ -15,11 +15,11 @@ namespace OptiFinance_System.Views;
 public partial class balanceGeneral : Form
 {
     // Declaración de variables globales
-    private BalanceGeneral total_activo_corriente;
-    private BalanceGeneral total_pasivo_corriente;
-    private BalanceGeneral total_activos_no_corrientes;
-    private BalanceGeneral total_pasivos_no_corrientes;
-    private BalanceGeneral total_capital;
+    private BalanceGeneral? _totalActivoCorriente;
+    private BalanceGeneral? _totalPasivoCorriente;
+    private BalanceGeneral? _totalActivosNoCorrientes;
+    private BalanceGeneral? _totalPasivosNoCorrientes;
+    private BalanceGeneral? _totalCapital;
     
     public balanceGeneral()
     {
@@ -50,146 +50,137 @@ public partial class balanceGeneral : Form
         // --> todas las cuentas
         List<BalanceGeneral> lista_cuentas = BalanceGeneralQuery.Instance.SelectAll();
         
-        // -> cuentas de activos
+        // -> cuentas de activosx
         List<BalanceGeneral> lista_cuenta_activo = BalanceGeneralQuery.Instance.SelectAllByTypeActivo();
-        List<BalanceGeneral> lista_cuenta_activo_corriente = BalanceGeneralQuery.Instance.SelectAllBytYpeActivoCorriente();
-        List<BalanceGeneral> lista_cuenta_activo_no_corriente = BalanceGeneralQuery.Instance.SelectAllByTypeActivoNoCorriente();
+        List<BalanceGeneral> listaCuentaActivoCorriente = BalanceGeneralQuery.Instance.SelectAllBytYpeActivoCorriente();
+        List<BalanceGeneral> listaCuentaActivoNoCorriente = BalanceGeneralQuery.Instance.SelectAllByTypeActivoNoCorriente();
         
         // --> cuentas de pasivos + capital
         List<BalanceGeneral> lista_cuenta_pasivo = BalanceGeneralQuery.Instance.SelectAllByTypePasivo();
-        List<BalanceGeneral> lista_cuenta_pasivo_corriente = BalanceGeneralQuery.Instance.SelectAllByTypePasivoCorriente();
-        List<BalanceGeneral> lista_cuenta_pasivo_no_corriente = BalanceGeneralQuery.Instance.SelectAllByTypePasivoNoCorriente();
-        List<BalanceGeneral> lista_cuenta_capital = BalanceGeneralQuery.Instance.SelectAllByTypeCapital();
+        List<BalanceGeneral> listaCuentaPasivoCorriente = BalanceGeneralQuery.Instance.SelectAllByTypePasivoCorriente();
+        List<BalanceGeneral> listaCuentaPasivoNoCorriente = BalanceGeneralQuery.Instance.SelectAllByTypePasivoNoCorriente();
+        List<BalanceGeneral> listaCuentaCapital = BalanceGeneralQuery.Instance.SelectAllByTypeCapital();
         
         // Calcular totales
         calcular_totales();
         
         // --> cuentas resultado deudora (4)
-        List<BalanceGeneral> lista_cuenta_resultado_deudora = BalanceGeneralQuery.Instance.SelectAllByTypeResultadoDeudor();
+        List<BalanceGeneral> listaCuentaResultadoDeudora = BalanceGeneralQuery.Instance.SelectAllByTypeResultadoDeudor();
         
         // --> cuentas resultado acreedor (5)
-        List<BalanceGeneral> lista_cuenta_resultado_acreedor = BalanceGeneralQuery.Instance.SelectAllByTypeResultadoAcreedor();
+        List<BalanceGeneral> listaCuentaResultadoAcreedor = BalanceGeneralQuery.Instance.SelectAllByTypeResultadoAcreedor();
         
         // --> cuentas puentes de cierre (6)
-        List<BalanceGeneral> lista_cuenta_puente_cierre = BalanceGeneralQuery.Instance.SelectAllByTypePuenteCierre();
+        List<BalanceGeneral> listaCuentaPuenteCierre = BalanceGeneralQuery.Instance.SelectAllByTypePuenteCierre();
         
         // Añadir encabezados principales para activos y pasivos en la misma fila
         dataBalance.Rows.Add("Activos", "", "Pasivos", "");
 
         // Crear índice para la siguiente fila donde se van a insertar subcategorías de Activos y Pasivos
-        int row_index = 1;
+        int rowIndex = 1;
 
         // Insertar secciones de Activos y Pasivos en paralelo
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "Activo Corriente", lista_cuenta_activo_corriente, "Pasivo Corriente", lista_cuenta_pasivo_corriente);
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "", lista_cuenta_resultado_deudora, "", lista_cuenta_resultado_acreedor); // Resultado Deudor y Acreedor
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "Total Activo Corriente", total_activo_corriente, "Total Pasivo Corriente", total_pasivo_corriente); // Resultado Deudor y Acreedor
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "Activo No Corriente", lista_cuenta_activo_no_corriente, "Pasivo No Corriente", lista_cuenta_pasivo_no_corriente);
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "Total Activo No Corriente", total_activos_no_corrientes, "Total Pasivo No Corriente", total_pasivos_no_corrientes);
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "", new List<BalanceGeneral>(), "Capital", lista_cuenta_capital);
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "", new List<BalanceGeneral>(), "", lista_cuenta_puente_cierre);
-        row_index = agregar_cuentas_filas(dataBalance, row_index, "", new List<BalanceGeneral>(), "Total de Capital", total_capital);
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "Activo Corriente", listaCuentaActivoCorriente, "Pasivo Corriente", listaCuentaPasivoCorriente);
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "", listaCuentaResultadoDeudora, "", listaCuentaResultadoAcreedor); // Resultado Deudor y Acreedor
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "Total Activo Corriente", _totalActivoCorriente, "Total Pasivo Corriente", _totalPasivoCorriente); // Resultado Deudor y Acreedor
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "Activo No Corriente", listaCuentaActivoNoCorriente, "Pasivo No Corriente", listaCuentaPasivoNoCorriente);
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "Total Activo No Corriente", _totalActivosNoCorrientes, "Total Pasivo No Corriente", _totalPasivosNoCorrientes);
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "", new List<BalanceGeneral>(), "Capital", listaCuentaCapital);
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "", new List<BalanceGeneral>(), "", listaCuentaPuenteCierre);
+        rowIndex = agregar_cuentas_filas(dataBalance, rowIndex, "", new List<BalanceGeneral>(), "Total de Capital", _totalCapital);
 
-        txtTotalActivo.Text = (total_activo_corriente.Debe + total_activos_no_corrientes.Debe).ToString();
-        txtTotalPasyPatri.Text = (total_pasivo_corriente.Haber + total_pasivos_no_corrientes.Haber + total_capital.Haber).ToString();
+        txtTotalActivo.Text = (_totalActivoCorriente.Debe + _totalActivosNoCorrientes.Debe).ToString();
+        txtTotalPasyPatri.Text = (_totalPasivoCorriente.Haber + _totalPasivosNoCorrientes.Haber + _totalCapital.Haber).ToString();
     }
     
     private void calcular_totales()
     {
         // Calcular y almacenar los totales
-        total_activo_corriente = BalanceGeneralQuery.Instance.SumByTypeActivoCorrienteAndResultadoDeudor();
-        total_pasivo_corriente = BalanceGeneralQuery.Instance.SumByTypePasivoCorrienteAndResultadoAcreedor();
-        total_activos_no_corrientes = BalanceGeneralQuery.Instance.SumByTypeActivoNoCorriente();
-        total_pasivos_no_corrientes = BalanceGeneralQuery.Instance.SumByTypePasivoNoCorriente();
-        total_capital = BalanceGeneralQuery.Instance.SumByTypeCapitalAndPuenteCierre();
+        // total_activo_corriente = BalanceGeneralQuery.Instance.SumByTypeActivoCorrienteAndResultadoDeudor();
+        _totalActivoCorriente = BalanceGeneralQuery.Instance.SumByTypeActivoCorriente(true);
+        // total_pasivo_corriente = BalanceGeneralQuery.Instance.SumByTypePasivoCorrienteAndResultadoAcreedor();
+        _totalPasivoCorriente = BalanceGeneralQuery.Instance.SumByTypePasivoCorriente(true);
+        _totalActivosNoCorrientes = BalanceGeneralQuery.Instance.SumByTypeActivoNoCorriente();
+        _totalPasivosNoCorrientes = BalanceGeneralQuery.Instance.SumByTypePasivoNoCorriente();
+        // total_capital = BalanceGeneralQuery.Instance.SumByTypeCapitalAndPuenteCierre();
+        _totalCapital = BalanceGeneralQuery.Instance.SumByTypeCapital(true);
     }
 
-    private int agregar_cuentas_filas(DataGridView dgv, int start_index, string titulo_activos, object cuentas_activos, string titulo_pasivos, object cuentas_pasivos)
+    private int agregar_cuentas_filas(DataGridView dgv, int startIndex, string tituloActivos, object cuentasActivos,
+        string tituloPasivos, object cuentasPasivos)
     {
         lblFechaBalance.Text = DateTime.Now.ToString("dd/MM/yyyy");
         lblFechaBalance.AutoSize = false;
         lblFechaBalance.TextAlign = ContentAlignment.MiddleCenter;
 
         // Agregar el título de la subcategoría de Activos y Pasivos en la misma fila
-        dgv.Rows.Insert(start_index, "   " + titulo_activos, "", "   " + titulo_pasivos, "");
-        start_index++;
+        dgv.Rows.Insert(startIndex, "   " + tituloActivos, "", "   " + tituloPasivos, "");
+        startIndex++;
 
         // Verificar si cuentasActivos es una lista o un valor único
-        if (cuentas_activos is List<BalanceGeneral> activos_list)
+        if (cuentasActivos is List<BalanceGeneral> activos_list)
         {
             // Insertar cada cuenta en la lista de activos
-            foreach (var cuenta in activos_list)
+            foreach (BalanceGeneral cuenta in activos_list)
             {
-                var row = new DataGridViewRow();
+                DataGridViewRow row = new();
                 row.CreateCells(dgv);
                 row.Cells[0].Value = "      " + cuenta.NombreCuenta;
                 row.Cells[1].Value = cuenta.Debe;
-                dgv.Rows.Insert(start_index, row);
-                start_index++;
+                dgv.Rows.Insert(startIndex, row);
+                startIndex++;
             }
         }
-        else if (cuentas_activos is BalanceGeneral total_activo)
+        else if (cuentasActivos is BalanceGeneral total_activo)
         {
             // Insertar directamente en las filas específicas para Activo Corriente y Activo No Corriente
             DataGridViewRow row_activo_corriente = dgv.Rows.Cast<DataGridViewRow>()
                 .FirstOrDefault(r => r.Cells[0].Value?.ToString().Trim() == "Total Activo Corriente");
 
-            if (row_activo_corriente != null)
-            {
-                row_activo_corriente.Cells[1].Value = total_activo_corriente.Debe;
-            }
+            if (row_activo_corriente != null) row_activo_corriente.Cells[1].Value = _totalActivoCorriente.Debe;
 
             DataGridViewRow row_activo_no_corriente = dgv.Rows.Cast<DataGridViewRow>()
                 .FirstOrDefault(r => r.Cells[0].Value?.ToString().Trim() == "Total Activo No Corriente");
 
             if (row_activo_no_corriente != null)
-            {
-                row_activo_no_corriente.Cells[1].Value = total_activos_no_corrientes.Debe;
-            }
+                row_activo_no_corriente.Cells[1].Value = _totalActivosNoCorrientes.Debe;
         }
 
         // Repite lo mismo para los Pasivos
-        if (cuentas_pasivos is List<BalanceGeneral> pasivos_list)
+        if (cuentasPasivos is List<BalanceGeneral> pasivos_list)
         {
-            int pasivo_index = 2;
-            foreach (var cuenta in pasivos_list)
+            int pasivoIndex = 2;
+            foreach (BalanceGeneral cuenta in pasivos_list)
             {
                 // if (dgv.Rows.Count <= pasivoIndex)
                 // {
                 //     dgv.Rows.Add(new DataGridViewRow());
                 // }
 
-                dgv.Rows[pasivo_index].Cells[2].Value = "      " + cuenta.NombreCuenta;
-                dgv.Rows[pasivo_index].Cells[3].Value = cuenta.Haber;
-                pasivo_index++;
+                dgv.Rows[pasivoIndex].Cells[2].Value = "      " + cuenta.NombreCuenta;
+                dgv.Rows[pasivoIndex].Cells[3].Value = cuenta.Haber;
+                pasivoIndex++;
             }
         }
-        else if (cuentas_pasivos is BalanceGeneral total_pasivo)
+        else if (cuentasPasivos is BalanceGeneral total_pasivo)
         {
-            DataGridViewRow row_pasivo_corriente = dgv.Rows.Cast<DataGridViewRow>()
+            DataGridViewRow rowPasivoCorriente = dgv.Rows.Cast<DataGridViewRow>()
                 .FirstOrDefault(r => r.Cells[2].Value?.ToString().Trim() == "Total Pasivo Corriente");
 
-            if (row_pasivo_corriente != null)
-            {
-                row_pasivo_corriente.Cells[3].Value = total_pasivo_corriente.Haber;
-            }
+            if (rowPasivoCorriente != null) rowPasivoCorriente.Cells[3].Value = _totalPasivoCorriente.Haber;
 
-            DataGridViewRow row_pasivo_no_corriente = dgv.Rows.Cast<DataGridViewRow>()
+            DataGridViewRow rowPasivoNoCorriente = dgv.Rows.Cast<DataGridViewRow>()
                 .FirstOrDefault(r => r.Cells[2].Value?.ToString().Trim() == "Total Pasivo No Corriente");
 
-            if (row_pasivo_no_corriente != null)
-            {
-                row_pasivo_no_corriente.Cells[3].Value = total_pasivos_no_corrientes.Haber;
-            }
+            if (rowPasivoNoCorriente != null)
+                rowPasivoNoCorriente.Cells[3].Value = _totalPasivosNoCorrientes.Haber;
 
-            DataGridViewRow row_total_capital = dgv.Rows.Cast<DataGridViewRow>()
+            DataGridViewRow rowTotalCapital = dgv.Rows.Cast<DataGridViewRow>()
                 .FirstOrDefault(r => r.Cells[2].Value?.ToString().Trim() == "Total de Capital");
 
-            if (row_total_capital != null)
-            {
-                row_total_capital.Cells[3].Value = total_capital.Haber;
-            }
+            if (rowTotalCapital != null) rowTotalCapital.Cells[3].Value = _totalCapital.Haber;
         }
-        
-        return start_index;
+
+        return startIndex;
     }
 }
--- OptiFinance_System/database/query/BalanceGeneralQuery.cs
+++ OptiFinance_System/database/query/BalanceGeneralQuery.cs
@@ -12,54 +12,79 @@ public class BalanceGeneralQuery
 {
     private static readonly Lazy<BalanceGeneralQuery> _instance = new(() => new());
     private readonly Connection _connectionInstance;
-    
+
     private BalanceGeneralQuery()
     {
         _connectionInstance = Connection.Instance;
         _connectionInstance.OpenConnection();
     }
-    
+
     public static BalanceGeneralQuery Instance => _instance.Value;
     private static BalanceGeneralParams Params => Queries.BalanceGeneralParams;
-    
-    
+
+
+    /// <summary>
+    /// Listado de todas las cuentas junto a sus saldos en el lado del debe o el haber, dependiendo de la cuenta
+    /// </summary>
+    /// <returns>
+    /// Una <see cref="List{T}"/> de tipo <see cref="BalanceGeneral"/>
+    /// </returns>
     public List<BalanceGeneral> SelectAll()
     {
         return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAll,
             Map, Params.ParametersSelectAll());
     }
-    
-    public List<BalanceGeneral> SelectAllByTypeActivo()
+
+    public List<BalanceGeneral> SelectAllByTypeActivo(bool selectResultadoDeudor = false)
     {
-        return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
-            Params.ParametersSelectAllByType(TypesAccount.Activo));
+        List<BalanceGeneral> byTypeActivo = QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(),
+            Params.SqlSelectAllByType, Map, Params.ParametersSelectAllByType(TypesAccount.Activo));
+        return !selectResultadoDeudor ? byTypeActivo : SelectAllByTypeActivoAndResultadoDeudor();
     }
 
-    public List<BalanceGeneral> SelectAllBytYpeActivoCorriente(bool selectResultadoAcreedor = false)
+    public List<BalanceGeneral> SelectAllByTypeActivoAndResultadoDeudor()
+    {
+        List<BalanceGeneral> byTypeActivo = SelectAllByTypeActivo();
+        List<BalanceGeneral> byTypeResultadoDeudor = SelectAllByTypeResultadoDeudor();
+        byTypeActivo.AddRange(byTypeResultadoDeudor);
+        return byTypeActivo;
+    }
+
+    public List<BalanceGeneral> SelectAllBytYpeActivoCorriente(bool selectResultadoDeudor = false)
     {
         List<BalanceGeneral> byTypeActivocorriente = QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(),
             Params.SqlSelectAllByType, Map, Params.ParametersSelectAllByType(TypesAccount.ActivoCorriente));
-        return !selectResultadoAcreedor ? byTypeActivocorriente : SelectAllBytYpeActivoCorrienteAndResultadoDeudor();
+        return !selectResultadoDeudor ? byTypeActivocorriente : SelectAllBytYpeActivoCorrienteAndResultadoDeudor();
     }
-    
-    public List<BalanceGeneral> SelectAllBytYpeActivoCorrienteAndResultadoDeudor()
+
+    private List<BalanceGeneral> SelectAllBytYpeActivoCorrienteAndResultadoDeudor()
     {
         List<BalanceGeneral> byTypeActivoCorriente = SelectAllBytYpeActivoCorriente();
         List<BalanceGeneral> byTypeResultadoDeudor = SelectAllByTypeResultadoDeudor();
         byTypeActivoCorriente.AddRange(byTypeResultadoDeudor);
         return byTypeActivoCorriente;
     }
-    
+
     public List<BalanceGeneral> SelectAllByTypeActivoNoCorriente()
     {
         return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
             Params.ParametersSelectAllByType(TypesAccount.ActivoNoCorriente));
     }
-    
-    public List<BalanceGeneral> SelectAllByTypePasivo()
+
+    public List<BalanceGeneral> SelectAllByTypePasivo(bool selectResultadoAcreedor = false)
     {
-        return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
+        List<BalanceGeneral> byTypePasivo = QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(),
+            Params.SqlSelectAllByType, Map,
             Params.ParametersSelectAllByType(TypesAccount.Pasivo));
+        return !selectResultadoAcreedor ? byTypePasivo : SelectAllByTyePasivoAndResultadoAcreedor();
+    }
+
+    private List<BalanceGeneral> SelectAllByTyePasivoAndResultadoAcreedor()
+    {
+        List<BalanceGeneral> byTypePasivo = SelectAllByTypePasivo();
+        List<BalanceGeneral> byTypeResultadoAcreedor = SelectAllByTypeResultadoAcreedor();
+        byTypePasivo.AddRange(byTypeResultadoAcreedor);
+        return byTypePasivo;
     }
 
     public List<BalanceGeneral> SelectAllByTypePasivoCorriente(bool selectResultadoAcreedor = false)
@@ -68,7 +93,7 @@ public class BalanceGeneralQuery
             Params.SqlSelectAllByType, Map, Params.ParametersSelectAllByType(TypesAccount.PasivoCorriente));
         return !selectResultadoAcreedor ? byTypePasivoCorriente : SelectByTypePasivoCorrienteAndResultadoAcreedor();
     }
-    
+
     private List<BalanceGeneral> SelectByTypePasivoCorrienteAndResultadoAcreedor()
     {
         List<BalanceGeneral> byTypePasivoCorriente = SelectAllByTypePasivoCorriente();
@@ -76,7 +101,7 @@ public class BalanceGeneralQuery
         byTypePasivoCorriente.AddRange(byTypeResultadoAcreedor);
         return byTypePasivoCorriente;
     }
-    
+
     public List<BalanceGeneral> SelectAllByTypePasivoNoCorriente()
     {
         return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
@@ -89,45 +114,52 @@ public class BalanceGeneralQuery
             Params.SqlSelectAllByType, Map, Params.ParametersSelectAllByType(TypesAccount.Capital));
         return !selectPuenteCierre ? byTypeCapital : SelctAllByTypeCapitalAndPuenteCierre();
     }
-    
-    public List<BalanceGeneral> SelctAllByTypeCapitalAndPuenteCierre()
+
+    private List<BalanceGeneral> SelctAllByTypeCapitalAndPuenteCierre()
     {
         List<BalanceGeneral> byTypeCapital = SelectAllByTypeCapital();
         List<BalanceGeneral> byTypePuenteCierre = SelectAllByTypePuenteCierre();
         byTypeCapital.AddRange(byTypePuenteCierre);
         return byTypeCapital;
     }
-    
+
     public List<BalanceGeneral> SelectAllByTypeResultadoDeudor()
     {
         return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
             Params.ParametersSelectAllByType(TypesAccount.CuentasDeResultadoDeudora));
     }
-    
+
     public List<BalanceGeneral> SelectAllByTypeResultadoAcreedor()
     {
         return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
             Params.ParametersSelectAllByType(TypesAccount.CuentasDeResultadoAcreedora));
     }
-    
+
     public List<BalanceGeneral> SelectAllByTypePuenteCierre()
     {
         return QueryHelper.ExecuteSelect(_connectionInstance.GetSqlConnection(), Params.SqlSelectAllByType, Map,
             Params.ParametersSelectAllByType(TypesAccount.CuentasDePuenteDeCierre));
     }
-    
-    public BalanceGeneral? SumByTypeActivo()
-    {
-        return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
-            Params.ParametersSumByType(TypesAccount.Activo));
-    }
-    
+
     private BalanceGeneral? SumInBalanceGeneral(BalanceGeneral? balanceGeneral1, BalanceGeneral? balanceGeneral2)
     {
         if (balanceGeneral1 == null || balanceGeneral2 == null) return null;
         balanceGeneral1.Debe += balanceGeneral2.Debe;
         balanceGeneral1.Haber += balanceGeneral2.Haber;
         return balanceGeneral1;
+    }
+
+    public BalanceGeneral? SumByTypeActivo(bool selectResultadoDeudor = false)
+    {
+        BalanceGeneral? byTypeActivo = QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(),
+            Params.SqlSumByType, Map,
+            Params.ParametersSumByType(TypesAccount.Activo));
+        return !selectResultadoDeudor ? byTypeActivo : SumByTypeActivoAndResultadoDeudor();
+    }
+
+    private BalanceGeneral? SumByTypeActivoAndResultadoDeudor()
+    {
+        return SumInBalanceGeneral(SumByTypeActivo(), SumByTypeResultadoDeudor());
     }
 
     public BalanceGeneral? SumByTypeActivoCorriente(bool selectResultadoDeudor = false)
@@ -137,36 +169,42 @@ public class BalanceGeneralQuery
         return !selectResultadoDeudor ? byTypeActivoCorriente : SumByTypeActivoCorrienteAndResultadoDeudor();
     }
 
-    public BalanceGeneral? SumByTypeActivoCorrienteAndResultadoDeudor()
+    private BalanceGeneral? SumByTypeActivoCorrienteAndResultadoDeudor()
     {
         return SumInBalanceGeneral(SumByTypeActivoCorriente(), SumByTypeResultadoDeudor());
     }
-    
+
     public BalanceGeneral? SumByTypeActivoNoCorriente()
     {
         return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
             Params.ParametersSumByType(TypesAccount.ActivoNoCorriente));
     }
-    
-    public BalanceGeneral? SumByTypePasivo()
+
+    public BalanceGeneral? SumByTypePasivo(bool selectResultadoAcreedor = false)
     {
-        return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
-            Params.ParametersSumByType(TypesAccount.Pasivo));
+        BalanceGeneral? byTypePasivo = QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(),
+            Params.SqlSumByType, Map, Params.ParametersSumByType(TypesAccount.Pasivo));
+        return !selectResultadoAcreedor ? byTypePasivo : SumByTypePasivoAndResultadoAcreedor();
     }
-    
+
+    private BalanceGeneral? SumByTypePasivoAndResultadoAcreedor()
+    {
+        return SumInBalanceGeneral(SumByTypePasivo(), SumByTypeResultadoAcreedor());
+    }
+
     public BalanceGeneral? SumByTypePasivoCorriente(bool selectResultadoAcreedor = false)
     {
         BalanceGeneral? byTypePasivoCorriente = QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(),
             Params.SqlSumByType, Map, Params.ParametersSumByType(TypesAccount.PasivoCorriente));
-        
+
         return !selectResultadoAcreedor ? byTypePasivoCorriente : SumByTypePasivoCorrienteAndResultadoAcreedor();
     }
-    
-    public BalanceGeneral? SumByTypePasivoCorrienteAndResultadoAcreedor()
+
+    private BalanceGeneral? SumByTypePasivoCorrienteAndResultadoAcreedor()
     {
         return SumInBalanceGeneral(SumByTypePasivoCorriente(), SumByTypeResultadoAcreedor());
     }
-    
+
     public BalanceGeneral? SumByTypePasivoNoCorriente()
     {
         return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
@@ -179,30 +217,30 @@ public class BalanceGeneralQuery
             Params.SqlSumByType, Map, Params.ParametersSumByType(TypesAccount.Capital));
         return !selectPuenteCierre ? byTypeCapital : SumByTypeCapitalAndPuenteCierre();
     }
-    
-    public BalanceGeneral? SumByTypeCapitalAndPuenteCierre()
+
+    private BalanceGeneral? SumByTypeCapitalAndPuenteCierre()
     {
         return SumInBalanceGeneral(SumByTypeCapital(), SumByTypePuenteCierre());
     }
-    
+
     public BalanceGeneral? SumByTypeResultadoDeudor()
     {
         return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
             Params.ParametersSumByType(TypesAccount.CuentasDeResultadoDeudora));
     }
-    
+
     public BalanceGeneral? SumByTypeResultadoAcreedor()
     {
         return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
             Params.ParametersSumByType(TypesAccount.CuentasDeResultadoAcreedora));
     }
-    
+
     public BalanceGeneral? SumByTypePuenteCierre()
     {
         return QueryHelper.ExecuteFind(_connectionInstance.GetSqlConnection(), Params.SqlSumByType, Map,
             Params.ParametersSumByType(TypesAccount.CuentasDePuenteDeCierre));
     }
-    
+
     public BalanceGeneral Map(SqlDataReader reader)
     {
         return Params.Map(reader);
