c1b2cd1d31bd2b7778dbf3df7b20383058d28d97
yurdeth <angelvasquez2570@gmail.com>
2024-11-04 15:43:35 -0600

    Manejando excepción de posible devisión por cero en razones_Liquidez

--- OptiFinance_System/Views/razones_Liquidez.cs
+++ OptiFinance_System/Views/razones_Liquidez.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,10 +27,14 @@ public partial class razones_Liquidez : Form
 
     private void btnRealizarOperacion_Click(object sender, EventArgs e)
     {
-        BalanceGeneral totalActivoCirculante = BalanceGeneralQuery.Instance.SumByTypeActivoCorriente(true);
-        BalanceGeneral totalPasivoCirculante = BalanceGeneralQuery.Instance.SumByTypePasivoCorriente(true);
+        BalanceGeneral? totalActivoCirculante = BalanceGeneralQuery.Instance.SumByTypeActivoCorriente();
+        BalanceGeneral? totalPasivoCirculante = BalanceGeneralQuery.Instance.SumByTypePasivoCorriente();
         List<BalanceGeneral> cuentasActivoCorriente = BalanceGeneralQuery.Instance.SelectAllBytYpeActivoCorriente();
-        BalanceGeneral inventario = cuentasActivoCorriente.FirstOrDefault(activo => activo.NombreCuenta == "Inventario");
+        BalanceGeneral? inventario = cuentasActivoCorriente.FirstOrDefault(activo => activo.NombreCuenta == "Inventario");
+
+        totalActivoCirculante ??= new(debe: 0, haber: 0);
+        totalPasivoCirculante ??= new(debe: 0, haber: 0);
+        inventario ??= new(debe: 0, haber: 0);
         
         // --> razon de circulante
         lblActivoCirculante.Text = totalActivoCirculante.Debe.ToString();
@@ -47,13 +52,15 @@ public partial class razones_Liquidez : Form
         try
         {
             // --> totales
-            lblTotalRcirculante.Text = (totalActivoCirculante.Debe / totalPasivoCirculante.Haber).ToString();
-            label16.Text = ((totalActivoCirculante.Debe - inventario.Debe) / totalPasivoCirculante.Haber).ToString();
+            // lblTotalRcirculante.Text = (totalActivoCirculante.Debe / totalPasivoCirculante.Haber).ToString();
+            lblTotalRcirculante.Text = Division(totalActivoCirculante.Debe, totalPasivoCirculante.Haber);
+            // label16.Text = ((totalActivoCirculante.Debe - inventario.Debe) / totalPasivoCirculante.Haber).ToString();
+            label16.Text = Division(totalActivoCirculante.Debe - inventario.Debe, totalPasivoCirculante.Haber);
             lblTptalCapTrabajo.Text = (totalActivoCirculante.Debe - totalPasivoCirculante.Haber).ToString();
         }
         catch (Exception ex)
         {
-            MessageBox.Show("Error: No se puede realizar las operaciones debido a una división por cero.");
+            MessageBox.Show(@"Error: No se puede realizar las operaciones debido a una división por cero.");
             lblActivoCirculante.Text = "";
             lblPasivoCirculante.Text = "";
             lblActCirculante.Text = "";
@@ -65,5 +72,14 @@ public partial class razones_Liquidez : Form
             label16.Text = "";
             lblTptalCapTrabajo.Text = "";
         }
+    }
+
+    private string? Division(decimal? dividendo, decimal? divisor)
+    {
+        if (dividendo == null || divisor == null)
+        {
+            return "Datos insuficientes para realizar esta operacion";
+        }
+        return divisor == 0 ? "Datos insuficientes" : (dividendo / divisor).ToString();
     }
 }
--- OptiFinance_System/database/models/BalanceGeneral.cs
+++ OptiFinance_System/database/models/BalanceGeneral.cs
@@ -6,4 +6,14 @@ public class BalanceGeneral
     public string? NombreCuenta;
     public decimal? Debe;
     public decimal? Haber;
+
+    public BalanceGeneral()
+    {
+    }
+
+    public BalanceGeneral(decimal? debe, decimal? haber)
+    {
+        Debe = debe;
+        Haber = haber;
+    }
 }
